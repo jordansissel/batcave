251f6dce66e9469596862ac97150a2c5c93f3bc9
Jordan Sissel <jls@semicomplete.com>
2015-07-01 15:23:02 -0700

    The great purge.

--- lib/batcave/namespace.rb
+++
@@ -1,6 +0,0 @@
-module BatCave
-  module Command; end
-  module Action; end
-  module Support; end # TODO(sissel): Support is a shitty namespace name.
-  THINGSDIR = File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "things"))
-end
